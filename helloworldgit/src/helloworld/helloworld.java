1d0d14ebc06aff46fc61f3aa8a4c00d016ab7679
HP <HP@HP-PC.lan>
2017-03-01 13:33:42 +0530

    initial -checkin

---
+++ helloworldgit/src/helloworld/helloworld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HELLO world");
+		System.out.println("HELLO world");
+
+	}
+
+}
